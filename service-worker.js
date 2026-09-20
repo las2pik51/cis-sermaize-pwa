ef9606b2ee97a807dfd7d33f020c6b4adb15af82
las2pik51 <didier.langeron@free.fr>
2026-09-20 10:05:42 +0200

    Ajout de la structure PWA

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+﻿const CACHE_NAME = 'cis-sermaize-v1';
+
+self.addEventListener('install', function(event) {
+  console.log('CIS SERMAIZE - Service Worker installé');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('CIS SERMAIZE - Service Worker actif');
+  event.waitUntil(self.clients.claim());
+});
